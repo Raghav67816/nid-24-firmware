997e5d08601c690d1d4b96f9cc2459d3fce4107f
Raghav67816 <kumaraghav079@gmail.com>
2026-04-21 21:54:42 +0530

    fixed menu navigation

--- src/cboard.cpp
+++ src/cboard.cpp
@@ -10,15 +10,14 @@
 #define SCREEN_HEIGHT 64
 
 #define BTN_OK 2
-#define BTN_UP 3
-#define BTN_DOWN 4
+#define BTN_DOWN 3
+#define BTN_UP 4
 
 Adafruit_SSD1306 display(
-    SCREEN_WIDTH, 
-    SCREEN_HEIGHT, 
-    &Wire, 
-    -1
-);
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    &Wire,
+    -1);
 
 // Page indexes:
 // 0 -> home
@@ -28,11 +27,13 @@ Adafruit_SSD1306 display(
 int page_index = 0;
 
 // menu
-const char* items[] = {"DEVICES", "DISCONNECT", "BACK"};
+const char *items[] = {"DEVICES", "DISCONNECT", "BACK"};
 const int menu_item_spacing = 15;
-int active_item = 0;  // devices -> 0, disconnect -> 1, back -> 2
+int active_item = 0; // devices -> 0, disconnect -> 1, back -> 2
+size_t item_len = strlen(*items);
 
-void setup(){
+void setup()
+{
     Serial.begin(9600);
     Wire.begin();
 
@@ -40,42 +41,44 @@ void setup(){
     pinMode(BTN_DOWN, INPUT_PULLUP);
     pinMode(BTN_OK, INPUT_PULLUP);
 
-
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
         Serial.println("failed to init display module.");
     }
     Serial.println("display initialized");
 
-
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
-
 }
 
-void draw_home(){
+void draw_home()
+{
     display.clearDisplay();
     display.setCursor(0, 10);
     display.println("CONNECTION STAT: ");
     display.display();
 }
 
-void draw_menu(int active_item){
+void draw_menu(int active_item)
+{
     display.clearDisplay();
-    display.setCursor((SCREEN_WIDTH/2)-strlen("MENU"), 0);
+    display.setCursor((SCREEN_WIDTH / 2) - strlen("MENU"), 0);
     display.fillRect(0, 0, SCREEN_WIDTH, 10, WHITE);
     display.setTextColor(SSD1306_BLACK);
     display.println("MENU");
 
     int prev_y = 5; // padding
-    for (int i=0; i < strlen(*items); i++){
+    for (int i = 0; i < strlen(*items); i++)
+    {
         prev_y += menu_item_spacing;
         display.setCursor(0, prev_y);
 
         display.setTextColor(SSD1306_WHITE);
-        
-        if(i == active_item){
-            display.fillRect(0, prev_y, 
-            SCREEN_WIDTH, 10, SSD1306_WHITE);
+
+        if (i == active_item)
+        {
+            display.fillRect(0, prev_y,
+                             SCREEN_WIDTH, 10, SSD1306_WHITE);
             display.setTextColor(SSD1306_BLACK);
         }
         display.println(items[i]);
@@ -84,14 +87,82 @@ void draw_menu(int active_item){
     display.display();
 }
 
+void loop()
+{
 
-void loop(){
-    draw_menu(active_item);
+    int btn_ok = digitalRead(BTN_OK);
+    int btn_down = digitalRead(BTN_DOWN);
+    int btn_up = digitalRead(BTN_UP);
 
-    if(active_item > 2) active_item = 0;
+    switch (page_index)
+    {
+    case 0:
+        draw_home();
+        break;
 
-    if(digitalRead(BTN_UP) == LOW){
-        Serial.println("Pressed");
-        active_item += 1;
+    case 1:
+        draw_menu(active_item);
+        break;
+
+    default:
+        break;
+    }
+
+    if (btn_ok == LOW)
+    {
+        switch (page_index)
+        {
+        case 0:
+            page_index = 1;
+            draw_menu(active_item);
+            break;
+
+        case 1:
+            // get the active item
+            Serial.println(items[active_item]);
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    if (btn_down == LOW)
+    {
+        switch (page_index)
+        {
+        case 0:
+            break;
+
+        case 1:
+            active_item -= 1;
+            if(active_item < 0){
+                active_item = 0;
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    // btn down
+    if (btn_up == LOW)
+    {
+        switch (page_index)
+        {
+        case 0:
+            break;
+
+        case 1:
+            active_item += 1;
+            if(active_item > 2){
+                active_item = 0;
+            }
+            draw_menu(active_item);
+
+        default:
+            break;
+        }
     }
 }
